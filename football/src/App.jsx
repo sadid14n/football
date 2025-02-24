3916bf6b6273c7a86d8723e0fb3e3425cd06a0cc
Sadid Alam <123662305+sadid14n@users.noreply.github.com>
2025-02-24 19:41:11 +0530

    Google Analytics Add

--- football/src/App.jsx
+++ football/src/App.jsx
@@ -2,8 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Admin from "./pages/Admin";
 import { GameProvider } from "./context/GameContext";
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-DBR1NEKYL5");
+    ReactGA.send("pageview");
+  }, []);
+
   return (
     <GameProvider>
       <Routes>
