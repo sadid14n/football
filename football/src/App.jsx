9690912ffaa501b8bf329ff1035bc8a98195e120
Sadid Alam <123662305+sadid14n@users.noreply.github.com>
2025-02-24 20:04:58 +0530

    Adding Google Analytics

--- football/src/App.jsx
+++ football/src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import Admin from "./pages/Admin";
 import { GameProvider } from "./context/GameContext";
 import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
+const TRACKING_ID = "G-Z1SMDCG39L";
 function App() {
   useEffect(() => {
-    ReactGA.initialize("G-DBR1NEKYL5");
-    ReactGA.send("pageview");
+    ReactGA.initialize(TRACKING_ID, { debug: true });
+    ReactGA.send("pageview"); // Ensures the first page load is tracked
   }, []);
-
   return (
     <GameProvider>
       <Routes>
